b460656399d5086b31c8fdd57dc835c926da3fcb
Tom <tmc48@cam.ac.uk>
2017-07-20 16:13:54 +0100

    Masters_Folder

--- Test/LinspaceTest.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-N=6
-Generation = np.linspace(1, N , N)
-print(Generation)
--- Test/test2.py
+++
@@ -1,5 +0,0 @@
-from Funcs import *
-from math import *
-import matplotlib.pyplot as plt
-EllipsePlot(1,0.5,pi/3)
-plt.show()
